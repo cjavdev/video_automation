9a6a9b927cae9755f20cc57bcad021ed8aa1b928
CJ Avilla <cjavilla@gmail.com>
2021-02-28 15:53:42 -0800

    Remove confirmable trait

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable
+         :lockable, :trackable
 end
